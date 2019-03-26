ce9762f9f2c82c0e8fc9429e17be241d21830f78
jakobschoepe <jakobschoepe@protonmail.com>
2019-03-26 16:20:13 +0100

    Update peims-class.R

--- R/peims-class.R
+++ R/peims-class.R
@@ -1,6 +1,8 @@
 #' @title S4 Class \code{"peims"}
-#' @description empty
+#' @slot seedi A matrix containing the state of L'Ecuyer's pseudo-random number generator from each replication.
+#' @slot oir A matrix containing the frequency of draws per observation from each replication.
+#' @slot betaij A matrix containing the estimated model parameters from each replication.
 #' @import data.table methods parallel pbapply
 #' @export
 
-setClass(Class = "peims", slots = c(oir = "matrix", betaij = "matrix"))
+setClass(Class = "peims", slots = c(seedi = "matrix", oir = "matrix", betaij = "matrix"))

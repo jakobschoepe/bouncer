c20cbc1b3093c605e0d7d11e2ae847e97e36caf5
jakobschoepe <jakobschoepe@protonmail.com>
2020-03-24 16:16:37 +0100

    Update subset-method.R

--- R/subset-method.R
+++ R/subset-method.R
@@ -1,11 +1,12 @@
 #' @title Subsetting a model obtained from resampling
-#' @description For objects of class \code{"peims"}, \code{subset} returns a model of interest.
+#' @description For objects of class \code{"peims"}, \code{subset()} returns a model of interest.
 #' @param x An object of class \code{"peims"}.
 #' @param model An integer giving the index of the observed ranking of the model of interest among all unique models (\code{model = 1} for the most frequent unique model).
 #' @return An object of S4 class \code{"peims"} containing the following slots:
 #' \item{oir}{A matrix containing the number of draws per observation in each resampling replicate with the model of interest.}
-#' \item{betaij}{A matrix containing the estimated model parameters of each resampling replicate with the model of interest.}
-#' @references Work in progress.
+#' \item{betaij}{A matrix containing the model parameter estimates of each resampling replicate with the model of interest.}
+#' @aliases coef,peims-method
+#' @author Jakob Schöpe
 #' @examples
 #' f <- function(data) {
 #' null <- glm(formula = y ~ 1, family = binomial, data = data)

35a97dac6fbae3c08d715e44a1c0aa4082ec0437
Jakob Schöpe <jakobschoepe@protonmail.com>
2021-10-14 18:06:36 +0200

    Update resample.R

--- R/resample.R
+++ R/resample.R
@@ -6,6 +6,7 @@
 #' @param size A positive integer giving the number of observations to draw from the original sample.
 #' @param replace A logical constant indicating if resampling should be with replacement.
 #' @param seed An optional integer vector giving the state of Pierre L'Ecuyer's pseudo-random number generator for reproducibility purposes.
+#' @param method
 #' @details \code{resample} is primarily utilized as a helper function in \code{peims}, but is also suitable for reproducibility purposes.
 #' @return A list containing the following elements:
 #' \item{seed}{An integer vector giving the state of Pierre L'Ecuyer's pseudo-random number generator that was used to resample the original sample.}
@@ -15,7 +16,7 @@
 #' @author Jakob Schöpe
 #' @export
 
-resample <- function(i, data, size, replace, seed) {
+resample <- function(i, data, size, replace, seed, method) {
   # If the argument 'seed' is provided, set the kind and the state of the pseudo-random number generator.
   if (!missing(x = seed)) {
     if (is.integer(x = seed) & length(x = seed) == 7L) {
@@ -27,6 +28,17 @@ resample <- function(i, data, size, replace, seed) {
       stop("\"seed\" has been misspecified")
     }
   }
+  else if (!is.character(x = method)) {
+    stop("\"method\" must be a character string")
+  }
+  
+  else if (length(x = method) != 1L) {
+    stop("single character string for \"method\" expected")
+  }
+  
+  else if (!(method %in% c("simple", "block"))) {
+    stop("\"method\" is misspecified")
+  }
   
   else if (!exists(x = ".Random.seed")) {
     stop("state for the pseudo-random number generator has not been set")
@@ -35,11 +47,19 @@ resample <- function(i, data, size, replace, seed) {
   else {  
     # Store the current state of the pseudo-random number generator for reproducability.
     seed <- .Random.seed
-  
-    # Resample from the original sample.
-    oir <- sample(x = 1:nrow(x = data), size = size, replace = replace)
-    data_tmp <- data[oir, ]
-
+    if (method == "simple") {
+      # Resample from the original sample.
+      oir <- sample(x = seq_len(nrow(data)), size = size, replace = replace)
+      data_tmp <- data[oir, ]
+    }
+    
+    if (method == "block") {
+      oir <- sample(x = unique(data$id), size = size, replace = replace)
+      id_tmp <- sapply(seq_len(length(oir)), function(i) {which(data$id == oir[i])})
+      data_tmp <- data[id_tmp, ]
+      data_tmp$id <- rep(seq_len(ncol(id_tmp)), each = nrow(id_tmp))
+    }
+    
     # Fit the user-defined model to the resampled data set. 
     betaij <- f(data = data_tmp)
   

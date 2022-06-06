7c6ebdd387cff5d5d332f7bb1a6d911f13b4c94b
Jakob Schöpe <jakobschoepe@protonmail.com>
2022-06-06 19:23:03 +0200

    Update resample.R

--- R/resample.R
+++ R/resample.R
@@ -1,5 +1,5 @@
-#' @title Resampling methods for \code{peims}.
-#' @description \code{resample} is a helper function for \code{peims} to resample from the original sample and subsequently fit the user-defined model.
+#' @title Resampling methods for \code{bouncer}.
+#' @description \code{resample} is a helper function for \code{bouncer} to resample from the original sample and subsequently fit the user-defined model.
 #' @usage resample(i, data, size, replace, seed)
 #' @param i Index of the replication (can be ignored). 
 #' @param data A data frame or data table containing the variables in the model.
@@ -7,12 +7,11 @@
 #' @param replace A logical constant indicating if resampling should be with replacement.
 #' @param seed An optional integer vector giving the state of Pierre L'Ecuyer's pseudo-random number generator for reproducibility purposes.
 #' @param method
-#' @details \code{resample} is primarily utilized as a helper function in \code{peims}, but is also suitable for reproducibility purposes.
+#' @details \code{resample} is primarily utilized as a helper function in \code{bouncer}, but is also suitable for reproducibility purposes.
 #' @return A list containing the following elements:
 #' \item{seed}{An integer vector giving the state of Pierre L'Ecuyer's pseudo-random number generator that was used to resample the original sample.}
 #' \item{oir}{An integer vector giving the indices of resampled observations.}
 #' \item{betaij}{A real vector giving the estimated model parameters.}
-#' @references Work in progress.
 #' @author Jakob Schöpe
 #' @export
 

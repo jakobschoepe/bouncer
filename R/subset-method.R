16e68c3e9a1938c1004667d5aa9cff8e2428f07b
Jakob Schöpe <jakobschoepe@protonmail.com>
2022-06-06 19:25:15 +0200

    Update subset-method.R

--- R/subset-method.R
+++ R/subset-method.R
@@ -1,27 +1,16 @@
 #' @title Subsetting a model obtained from resampling
-#' @description For objects of class \code{"peims"}, \code{subset()} returns a model of interest.
-#' @param x An object of class \code{"peims"}.
+#' @description For objects of class \code{"bouncer"}, \code{subset()} returns a model of interest.
+#' @param x An object of class \code{"bouncer"}.
 #' @param model An integer giving the index of the observed ranking of the model of interest among all unique models (\code{model = 1} for the most frequent unique model).
-#' @return An object of S4 class \code{"peims"} containing the following slots:
+#' @return An object of S4 class \code{"bouncer"} containing the following slots:
 #' \item{oir}{A matrix containing the number of draws per observation in each resampling replicate with the model of interest.}
 #' \item{betaij}{A matrix containing the model parameter estimates of each resampling replicate with the model of interest.}
 #' @aliases coef,peims-method
 #' @author Jakob Schöpe
-#' @examples
-#' f <- function(data) {
-#' null <- glm(formula = y ~ 1, family = binomial, data = data)
-#' full <- glm(formula = y ~ ., family = binomial, data = data)
-#' fit <- coef(step(object = null, scope = list(upper = full), direction = "both", trace = 0, k = 2))
-#' return(fit)
-#' }
-#'
-#' fit <- peims(f = f, data = data, size = 100L, replace = TRUE, k = 5000L, seed = 123L, ncpus = 2L)
-#'
-#' subset(fit)
 #' @export
 
 setMethod(f = "subset",
-          signature = "peims",
+          signature = "bouncer",
           definition = function(x, model = 1) {
 
             # Cast matrices into data tables to subsequently subset more efficient

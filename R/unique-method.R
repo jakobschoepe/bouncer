f9e3e6e754d79156b3d3892a96e13403db6aa4ce
Jakob Schöpe <jakobschoepe@protonmail.com>
2022-06-06 19:24:05 +0200

    Update unique-method.R

--- R/unique-method.R
+++ R/unique-method.R
@@ -1,23 +1,11 @@
 #' @title Summarizing unique models
-#' @description For objects of class \code{"peims"}, \code{unique} summarizes unique models obtained from resampling.
-#' @param x An object of class \code{"peims"}.
+#' @description For objects of class \code{"bouncer"}, \code{unique} summarizes unique models obtained from resampling.
+#' @param x An object of class \code{"bouncer"}.
 #' @return A data table
-#' @references Work in progress.
-#' @examples
-#' f <- function(data) {
-#' null <- glm(formula = y ~ 1, family = binomial, data = data)
-#' full <- glm(formula = y ~ ., family = binomial, data = data)
-#' fit <- coef(step(object = null, scope = list(upper = full), direction = "both", trace = 0, k = 2))
-#' return(fit)
-#' }
-#'
-#' fit <- peims(f = f, data = data, size = 100L, replace = TRUE, k = 5000L, seed = 123L, ncpus = 2L)
-#'
-#' unique(fit)
 #' @export
 
 setMethod(f = "unique",
-          signature = "peims",
+          signature = "bouncer",
           definition = function(x) {
             betaij <- data.table::as.data.table(x = !is.na(x = x@betaij))
             betaij <- betaij[, .N, by = names(betaij)][order(N, decreasing = TRUE)]

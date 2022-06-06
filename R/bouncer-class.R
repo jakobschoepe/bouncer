8a0dd342240af10005d6103c71e58584d2dc0c2b
Jakob Schöpe <jakobschoepe@protonmail.com>
2022-06-06 17:57:31 +0200

    Update and rename peims-class.R to bouncer-class.R

--- R/bouncer-class.R
+++ R/bouncer-class.R
@@ -1,8 +1,8 @@
-#' @title S4 Class \code{"peims"}
+#' @title S4 Class \code{"bouncer"}
 #' @slot seedi A numeric matrix containing the state of L'Ecuyer's pseudo-random number generator from each resampling replication.
 #' @slot oir A numeric matrix containing the frequency of draws per observation from each resampling replication.
 #' @slot betaij A numeric matrix containing the model parameter estimates from each resampling replication.
 #' @import data.table methods parallel pbapply
 #' @export
 
-setClass(Class = "peims", slots = c(seedi = "matrix", oir = "matrix", betaij = "matrix"))
+setClass(Class = "bouncer", slots = c(seedi = "matrix", oir = "matrix", betaij = "matrix"))

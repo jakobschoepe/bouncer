2889de82c1695f797c49c2cf08f405a10ecc194b
Jakob Schöpe <jakobschoepe@protonmail.com>
2022-06-16 18:42:12 +0200

    Update test-peims-new.R

--- tests/testthat/test-peims-new.R
+++ tests/testthat/test-peims-new.R
@@ -5,8 +5,9 @@ X3 <- rnorm(100)
 X4 <- rbinom(100, 1, 0.5)
 b <- model.matrix(~ X1 + X2 + X3 + X4) %*% c(-2.10, 0.00, -0.45, 0.22, -0.16)
 Y <- rbinom(100, 1, 1 / (1 + exp(-b)))
-data <- data.frame(X1, X2, X3, X4, Y, id = rep(1:50, times = 2))
-f <- function(data, seed) {
+data01 <- data.frame(X1, X2, X3, X4, Y)
+data02 <- data.frame(X1, X2, X3, X4, Y = rep(0:1, each = 50), id = rep(1:50, times = 2))
+f01 <- function(data, seed) {
   out <- tryCatch(expr = {
                     y <- data$Y
                     X <- as.matrix(subset(data, select = -c(Y, id)))
@@ -18,7 +19,7 @@ f <- function(data, seed) {
   return(out)
 }
 
-f2 <- function(data, seed) {
+f02 <- function(data, seed) {
   out <- tryCatch(expr = {
                     y <- glmnet::stratifySurv(y = Surv(start = rep(0, times = length(y)), stop = rep(1, times = length(y)), event = data$Y), strata = data$id)
                     X <- as.matrix(subset(data, select = -c(Y, id)))
@@ -33,67 +34,67 @@ f2 <- function(data, seed) {
 ncpus <- as.integer(x = parallel::detectCores())
 
 testthat::test_that(desc = "bouncer throws an error if arguments are misspecified", 
-                    code = {testthat::expect_error(object = bouncer(f = NULL, data = data, size = 100L, replace = TRUE, k = 100L, seed = 123L, ncpus = ncpus, method = "simple"),
+                    code = {testthat::expect_error(object = bouncer(f = NULL, data = data01, size = 100L, replace = TRUE, k = 100L, seed = 123L, ncpus = ncpus, method = "simple"),
                                                    regexp = "\"f\" must be a function")
                                
-                            testthat::expect_error(object = bouncer(f = function(){}, data = data, size = 100L, replace = TRUE, k = 100L, seed = 123L, ncpus = ncpus, method = "simple"),
+                            testthat::expect_error(object = bouncer(f = function(){}, data = data01, size = 100L, replace = TRUE, k = 100L, seed = 123L, ncpus = ncpus, method = "simple"),
                                                    regexp = "\"f\" must contain the following argument: \"data\"")
                                
                             testthat::expect_error(object = bouncer(f = f, data = NULL, size = 100L, replace = TRUE, k = 100L, seed = 123L, ncpus = ncpus, method = "simple"),
                                                    regexp = "\"data\" must be a data frame")
                                
-                            testthat::expect_error(object = bouncer(f = f, data = data, size = NULL, replace = TRUE, k = 100L, seed = 123L, ncpus = ncpus, method = "simple"),
+                            testthat::expect_error(object = bouncer(f = f, data = data01, size = NULL, replace = TRUE, k = 100L, seed = 123L, ncpus = ncpus, method = "simple"),
                                                    regexp = "\"size\" must be a positive integer")
                                
-                            testthat::expect_error(object = bouncer(f = f, data = data, size = rep(x = 100L, times = 2), replace = TRUE, k = 100L, seed = 123L, ncpus = ncpus, method = "simple"),
+                            testthat::expect_error(object = bouncer(f = f, data = data01, size = rep(x = 100L, times = 2), replace = TRUE, k = 100L, seed = 123L, ncpus = ncpus, method = "simple"),
                                                    regexp = "single positive integer for \"size\" expected")
                                
-                            testthat::expect_error(object = bouncer(f = f, data = data, size = 0L, replace = TRUE, k = 100L, seed = 123L, ncpus = ncpus, method = "simple"),
+                            testthat::expect_error(object = bouncer(f = f, data = data01, size = 0L, replace = TRUE, k = 100L, seed = 123L, ncpus = ncpus, method = "simple"),
                                                    regexp = "\"size\" must be a positive integer")
                                
-                            testthat::expect_error(object = bouncer(f = f, data = data, size = 101L, replace = TRUE, k = 100L, seed = 123L, ncpus = ncpus, method = "simple"),
+                            testthat::expect_error(object = bouncer(f = f, data = data01, size = 101L, replace = TRUE, k = 100L, seed = 123L, ncpus = ncpus, method = "simple"),
                                                    regexp = "\"size\" exceeds the number of available observations in \"data\"")
                                
-                            testthat::expect_error(object = bouncer(f = f, data = data, size = 100L, replace = NULL, k = 100L, seed = 123L, ncpus = ncpus, method = "simple"),
+                            testthat::expect_error(object = bouncer(f = f, data = data01, size = 100L, replace = NULL, k = 100L, seed = 123L, ncpus = ncpus, method = "simple"),
                                                    regexp = "\"replace\" must be a logical value")
                                
-                            testthat::expect_error(object = bouncer(f = f, data = data, size = 100L, replace = c(FALSE, TRUE), k = 100L, seed = 123L, ncpus = ncpus, method = "simple"),
+                            testthat::expect_error(object = bouncer(f = f, data = data01, size = 100L, replace = c(FALSE, TRUE), k = 100L, seed = 123L, ncpus = ncpus, method = "simple"),
                                                    regexp = "single logical value for \"replace\" expected")
                                
-                            testthat::expect_error(object = bouncer(f = f, data = data, size = 100L, replace = TRUE, k = NULL, seed = 123L, ncpus = ncpus, method = "simple"),
+                            testthat::expect_error(object = bouncer(f = f, data = data01, size = 100L, replace = TRUE, k = NULL, seed = 123L, ncpus = ncpus, method = "simple"),
                                                    regexp = "\"k\" must be a positive integer equal to or greater than 2")
                                
-                            testthat::expect_error(object = bouncer(f = f, data = data, size = 100L, replace = TRUE, k = rep(x = 100L, times = 2), seed = 123L, ncpus = ncpus, method = "simple"),
+                            testthat::expect_error(object = bouncer(f = f, data = data01, size = 100L, replace = TRUE, k = rep(x = 100L, times = 2), seed = 123L, ncpus = ncpus, method = "simple"),
                                                    regexp = "single positive integer for \"k\" expected")
                                
-                            testthat::expect_error(object = bouncer(f = f, data = data, size = 100L, replace = TRUE, k = 1L, seed = 123L, ncpus = ncpus, method = "simple"),
+                            testthat::expect_error(object = bouncer(f = f, data = data01, size = 100L, replace = TRUE, k = 1L, seed = 123L, ncpus = ncpus, method = "simple"),
                                                    regexp = "\"k\" must be a positive integer equal to or greater than 2")
                                
-                            testthat::expect_error(object = bouncer(f = f, data = data, size = 100L, replace = TRUE, k = 100L, seed = NULL, ncpus = ncpus, method = "simple"),
+                            testthat::expect_error(object = bouncer(f = f, data = data01, size = 100L, replace = TRUE, k = 100L, seed = NULL, ncpus = ncpus, method = "simple"),
                                                    regexp = "\"seed\" must be an integer")
                                
-                            testthat::expect_error(object = bouncer(f = f, data = data, size = 100L, replace = TRUE, k = 100L, seed = rep(x = 123L, times = 2), ncpus = ncpus, method = "simple"),
+                            testthat::expect_error(object = bouncer(f = f, data = data01, size = 100L, replace = TRUE, k = 100L, seed = rep(x = 123L, times = 2), ncpus = ncpus, method = "simple"),
                                                    regexp = "single integer for \"seed\" expected")
                                
-                            testthat::expect_error(object = bouncer(f = f, data = data, size = 100L, replace = TRUE, k = 100L, seed = 123L, ncpus = NULL, method = "simple"),
+                            testthat::expect_error(object = bouncer(f = f, data = data01, size = 100L, replace = TRUE, k = 100L, seed = 123L, ncpus = NULL, method = "simple"),
                                                    regexp = "\"ncpus\" must be a positive integer")
                                
-                            testthat::expect_error(object = bouncer(f = f, data = data, size = 100L, replace = TRUE, k = 100L, seed = 123L, ncpus = rep(x = ncpus, times = 2), method = "simple"),
+                            testthat::expect_error(object = bouncer(f = f, data = data01, size = 100L, replace = TRUE, k = 100L, seed = 123L, ncpus = rep(x = ncpus, times = 2), method = "simple"),
                                                    regexp = "single positive integer for \"ncpus\" expected")
                                
-                            testthat::expect_error(object = bouncer(f = f, data = data, size = 100L, replace = TRUE, k = 100L, seed = 123L, ncpus = 0L, method = "simple"),
+                            testthat::expect_error(object = bouncer(f = f, data = data01, size = 100L, replace = TRUE, k = 100L, seed = 123L, ncpus = 0L, method = "simple"),
                                                    regexp = "\"ncpus\" must be a positive integer")
                                
-                            testthat::expect_error(object = bouncer(f = f, data = data, size = 100L, replace = TRUE, k = 100L, seed = 123L, ncpus = ncpus + 1L, method = "simple"),
+                            testthat::expect_error(object = bouncer(f = f, data = data01, size = 100L, replace = TRUE, k = 100L, seed = 123L, ncpus = ncpus + 1L, method = "simple"),
                                                    regexp = "\"ncpus\" exceeds the number of detected cores")
                   
-                            testthat::expect_error(object = bouncer(f = f, data = data, size = 100L, replace = TRUE, k = 100L, seed = 123L, ncpus = ncpus, method = NULL),
+                            testthat::expect_error(object = bouncer(f = f, data = data01, size = 100L, replace = TRUE, k = 100L, seed = 123L, ncpus = ncpus, method = NULL),
                                                    regexp = "\"method\" must be a character string")
                   
-                            testthat::expect_error(object = bouncer(f = f, data = data, size = 100L, replace = TRUE, k = 100L, seed = 123L, ncpus = ncpus, method = c("simple", "block")),
+                            testthat::expect_error(object = bouncer(f = f, data = data01, size = 100L, replace = TRUE, k = 100L, seed = 123L, ncpus = ncpus, method = c("simple", "block")),
                                                    regexp = "single character string for \"method\" expected")
                   
-                            testthat::expect_error(object = bouncer(f = f, data = data, size = 100L, replace = TRUE, k = 100L, seed = 123L, ncpus = ncpus, method = "NULL"),
+                            testthat::expect_error(object = bouncer(f = f, data = data01, size = 100L, replace = TRUE, k = 100L, seed = 123L, ncpus = ncpus, method = "NULL"),
                                                    regexp = "\"method\" is misspecified")
           }
 )
